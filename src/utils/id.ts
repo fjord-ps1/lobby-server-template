b33b1289674496413e768c7917847b4e56b55a05
Fjord.ps1 <96792375+fjord-ps1@users.noreply.github.com>
2026-01-20 21:42:15 +0100

    Add functions to generate unique IDs

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,1 +1,8 @@
+export const generateId = (): string => {
+  return Math.random().toString(36).substring(2, 15) + 
+         Math.random().toString(36).substring(2, 15);
+};
 
+export const generateShortId = (): string => {
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
+};
