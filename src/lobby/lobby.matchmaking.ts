71a22d55a227585854e8d0edea0ea6a33bdf66fd
Fjord.ps1 <96792375+fjord-ps1@users.noreply.github.com>
2026-01-21 09:57:36 +0100

    Remove socket setup code from lobby matchmaking

--- src/lobby/lobby.matchmaking.ts
+++ src/lobby/lobby.matchmaking.ts
@@ -1,16 +1,1 @@
-import { Server } from 'socket.io';
-import { ClientEvents, ServerEvents } from '../types/events';
-import { registerHandlers } from './handlers';
-import { logger } from '../utils/logger';
 
-export const setupSocket = (io: Server<ClientEvents, ServerEvents>) => {
-  io.on('connection', (socket) => {
-    logger.info('Player connected', { socketId: socket.id });
-    
-    registerHandlers(io, socket);
-    
-    socket.on('disconnect', () => {
-      logger.info('Player disconnected', { socketId: socket.id });
-    });
-  });
-};
